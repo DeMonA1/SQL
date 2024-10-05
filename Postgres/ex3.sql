6bafbed8d3df862cfed7088db27125bd02e1ae25
Dzmitry <126611062+DeMonA1@users.noreply.github.com>
2024-10-05 12:51:49 -0400

    Update ex3.sql

--- Postgres/ex3.sql
+++ Postgres/ex3.sql
@@ -903,3 +903,34 @@ EXPLAIN SELECT book_ref FROM bookings WHERE book_ref < '000FFF'
 ORDER BY book_ref;
 EXPLAIN SELECT count(*) FROM seats WHERE aircraft_code = 'SU9';
 EXPLAIN SELECT avg(total_amount) FROM bookings;
+
+
+
+EXPLAIN SELECT a.aircraft_code, a.model,
+                s.seat_no, s.fare_conditions
+FROM seats s
+JOIN aircrafts a ON s.aircraft_code = a.aircraft_code
+WHERE a.model ~ '^Аэро' ORDER BY s.seat_no;
+
+EXPLAIN SELECT r.flight_no, r.departure_airport_name,
+                r.arrival_airport_name, a.model
+FROM routes r
+JOIN aircrafts a ON r.aircraft_code = a.aircraft_code
+ORDER BY flight_no;
+
+EXPLAIN SELECT t.ticket_no, t.passenger_name,
+                tf.flight_id, tf.amount
+FROM tickets t
+JOIN ticket_flights tf ON t.ticket_no = tf.ticket_no
+ORDER BY t.ticket_no;
+
+SET enable_hashjoin = on;
+SET enable_mergejoin = ON;
+SET enable_nestloop = on;
+
+EXPLAIN ANALYZE
+SELECT t.ticket_no, t.passenger_name,
+                tf.flight_id, tf.amount
+FROM tickets t
+JOIN ticket_flights tf ON t.ticket_no = tf.ticket_no
+ORDER BY t.ticket_no;
